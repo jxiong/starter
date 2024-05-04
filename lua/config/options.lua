370e3494461100734575f4951a68fdd95eb25616
Jinshan Xiong <jinshan.xiong@gmail.com>
2024-05-04 09:21:52 -0700

    customization

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = false
+vim.opt.mouse = ""
+
+--[[
+<C-\><C-n> is the standard way to get to Normal mode in Vim, regardless of the
+current mode. However, pressing control-backslash-control-n to get out of
+Terminal mode can be awkward. To make the <Esc> key have the same effect in
+Terminal mode, a snippet of Vim script can be put into the vimrc file.
+This snippet is:
+--]]
+vim.keymap.set("t", "<C-b>", [[<C-\><C-n>]])
+-- Pressing 'i' again will go back to the insert mode.
---
+++ lua/plugins/custom.lua
@@ -0,0 +1,39 @@
+return {
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "elflord",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        clangd = {
+          root_dir = function(fname)
+            require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+          filetypes = { "c", "h", "cpp", "hpp", "cc", "hh" },
+        },
+      },
+    },
+  },
+}
